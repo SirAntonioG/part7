a575bdf2e50d72e2f02ad32802977af03ac97e2c
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-24 09:10:35 -0500

    Fullstack part7 section c Navigation structure component props

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {
   Alert,
   AppBar,
   Toolbar,
-  IconButton,
+  // IconButton,
 } from "@mui/material";
 
 import {
@@ -165,23 +165,22 @@ const App = () => {
       {message && <Alert severity="success">{message}</Alert>}
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Button color="inherit">
-            <Link to="/">home</Link>
+          <Button color="inherit" component={Link} to="/">
+            home
           </Button>
-          <Button color="inherit">
-            <Link to="/notes">notes</Link>
+          <Button color="inherit" component={Link} to="/notes">
+            notes
           </Button>
-          <Button color="inherit">
-            <Link to="/users">users</Link>
+          <Button color="inherit" component={Link} to="/users">
+            users
           </Button>
-          <Button color="inherit">
-            {user ? <em>{user} logged in</em> : <Link to="/login">login</Link>}
-          </Button>
+          {user ? (
+            <em>{user} logged in</em>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
 
